cd2e9e9f893f788a68125c18afed8e067978630a
RuanPS01 <30608019+RuanPS01@users.noreply.github.com>
2021-04-28 11:24:40 -0300

    First Changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+import { expect } from "chai";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+var jsdom = require("mocha-jsdom");
+
+global.document = jsdom({
+  url: "http://localhost:3000/"
 });
+
+let rootContainer;
+
+beforeEach(() => {
+  rootContainer = document.createElement("div");
+  document.body.appendChild(rootContainer);
+});
+
+afterEach(() => {
+  document.body.removeChild(rootContainer);
+  rootContainer = null;
+});
+
+describe("App Component Testing", () => {
+  it("Renders Hello World Title", () => {
+    act(() => {
+      ReactDOM.render(<App />, rootContainer);
+    });
+    const h1 = rootContainer.querySelector("h1");
+    // eslint-disable-next-line jest/valid-expect
+    expect(h1.textContent).to.equal("Hello World");
+  });
+});
+
+// Command for test
+// ./node_modules/.bin/mocha --require babel-register ./src/App.test.js
